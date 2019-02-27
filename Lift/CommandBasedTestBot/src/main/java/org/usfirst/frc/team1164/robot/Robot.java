557c28b1b301dc070954564ba82d66945051f9eb
ElijahVela <evela@nmsu.edu>
2019-02-26 17:43:41 -0700

    Moved command
    Now you can only press the button when the robot's on teleop.  Before it was on robotperiodic, so, just to avoid incident

--- Lift/CommandBasedTestBot/src/main/java/org/usfirst/frc/team1164/robot/Robot.java
+++ Lift/CommandBasedTestBot/src/main/java/org/usfirst/frc/team1164/robot/Robot.java
@@ -71,7 +71,7 @@ public class Robot extends TimedRobot {
 	public void robotPeriodic(){
 
 		SmartDashboard.putNumber("Encoder Raw Values", kLift.getRaw());
-		SmartDashboard.putData("Make Lift Go Up", new MakeLiftGoUp());
+		
 	}
 
 	/**
@@ -142,6 +142,7 @@ public class Robot extends TimedRobot {
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
+		SmartDashboard.putData("Make Lift Go Up", new MakeLiftGoUp());
 	}
 
 	/**
